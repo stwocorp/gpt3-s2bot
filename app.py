249b8ff9eae3b6659fbd4d90aba35ecbd3acc8ee
stwocorp <stwocorp@gmail.com>
2021-08-07 23:35:57 +0200

    GPT3

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, session
+from twilio.twiml.messaging_response import MessagingResponse
+from s2bot import ask, append_interation_to_chat_log
+
+app = Flask(__name__)
+# if for some reason your conversation with the bot gets weirdm change the secret key
+app.config['SECRET_KEY']='89djhf9jhdk93'
+
+@app.route('s2bot', methods=['POST'])
+def s2():
+    incoming_msg = request.values['BODY']
+    chat_log = session.get('chat_log')
+    answer = ask(incoming_msg, chat_log)
+    session['chat_log'] = append_interation_to_chat_log(incoming_msg, answer, chat_log)
+    msg = MessagingResponse
+    msg.message(answer)
+    return str(msg)
+
+if __name__ = '__main__':
+    app.run(debug=True)
+    
